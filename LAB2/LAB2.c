82a139f0a3dd145023ce84f2f8a3af3a8a260443
IvanGovnov <IvanGovnov07@gmail.com>
2013-12-04 19:02:10 +0300

    fix initial connection

--- LAB2/LAB2.c
+++ LAB2/LAB2.c
@@ -9,13 +9,14 @@
 #include <string.h>
 
 
-#define	MAX_PENDING	1							// max pending client connections
-#define	BUFFER_SIZE  	1024							// for incomming
-#define ARG_ERROR_MESS	"CLIENT_MODE:\nclient <ip> <port> <filename or full filepath>\n\nSERVER_MODE:\nserver <ip> <port>"
+#define	MAX_PENDING		1						// max pending client connections
+#define	BUFFER_SIZE  		1024						// for incomming
+#define ARG_ERROR_MESS		"CLIENT_MODE:\nclient <ip> <port> <filename or full filepath>\n\nSERVER_MODE:\nserver <ip> <port>"
+#define	MAX_FILEPATH_LENGHT	64						// in bytes
 
 int workSock, listenSock;							// socket descriptors
 int ind = 0;									// indicator that any socket in open
-long long	fileSize = 1;
+long long	fileSize = 2;
 long long 	filePointer = 0;
 
 
@@ -70,7 +71,7 @@ int startServer(char *hostName, char *port)
 	int 		so_reuseaddr = 1;					// for setsockop SO_REUSEADDR set enable
 	int 		clientFirstPacket;					// client first packet indicator
 	FILE*		file;							
-	char*		filePath = (char*) malloc (64*sizeof(char));
+	char*		filePath = (char*) malloc (MAX_FILEPATH_LENGHT*sizeof(char));
 
     	hostAddr.sin_family = AF_INET;
     	hostAddr.sin_port = htons(atoi(port));					// convert host byte order -> network byte order		
@@ -107,7 +108,7 @@ int startServer(char *hostName, char *port)
 	struct timeval time_out; time_out.tv_sec = 0; time_out.tv_usec = 0;
 	while(1)								// if any key is pressed -> exit from while loop
     	{		
-		printf("server_accept\n");
+		printf("server_accept_wait_for_client\n");
 		clientFirstPacket = 1;
 		if(((workSock) = accept((listenSock), 0, 0)) < 0)		// wait for client
 		{
@@ -115,7 +116,7 @@ int startServer(char *hostName, char *port)
             		return -1;    
         	}
 		ind++;		
-       		while(filePointer < fileSize)							
+       		while(filePointer+1 < fileSize)							
         	{						
 			printf("server_select %lld %lld\n", filePointer, fileSize);
 			if(select(0,NULL,NULL,&temp,&time_out) < 0)
@@ -123,29 +124,29 @@ int startServer(char *hostName, char *port)
 				puts("server timeout 10s reached");
 				break;
 			}          
-			printf("server_recv data\n");
-			
-			if(clientFirstPacket)					// check if file exist (filename gets from client message) 
-			{			
-			if((readBytes = recv((workSock), (char*)filePath, 64*sizeof(char), 0)) < 0)
-										// receive filePath from client
-			{
-				perror("func recv");
-				return -1;
-			}
-			printf("server_readbytes %d\n", readBytes);
-			if((readBytes = recv((workSock), (char*)&fileSize, sizeof(long long), 0)) < 0)
-										// receive fileSize from client
-			{
-			  perror("func recv");
-			  return -1;
-			}
-			printf("server_recv fileSize %lld\n",  fileSize);
-			if(!readBytes)
-				break;
-			  printf("server_first_realloc\n");
+			printf("server_recv data\n");			
+			if(clientFirstPacket)					// if first packet from client
+										// check if file exist (filename gets from client message) 
+			{		
 			  clientFirstPacket = 0;
-			 // filePath = (char*) realloc (buf, (readBytes + 1)*sizeof(char));filePointer	
+			  if((readBytes = recv((workSock), (char*)filePath, MAX_FILEPATH_LENGHT*sizeof(char), 0)) < 0)
+										  // receive filePath from client
+			  {
+				  perror("func recv");
+				  return -1;
+			  }
+			  printf("server_readbytes num %d, message: %s\n", readBytes, filePath);
+			  if((readBytes = recv((workSock), (char*)&fileSize, sizeof(long long), 0)) < 0)
+										  // receive fileSize from client
+			  {
+			    perror("func recv");
+			    return -1;
+			  }
+			  printf("server_recv fileSize %lld\n",  fileSize);
+			  if(!readBytes)
+				  break;
+			  printf("server_first_realloc\n");			  
+			  // filePath = (char*) realloc (buf, (readBytes + 1)*sizeof(char));filePointer	
 			  filePath[readBytes] = '\0';
 			  printf("server_accept_filePath %s\n",  filePath);
 			  if(access(filePath, F_OK ) < 0)
@@ -162,28 +163,28 @@ int startServer(char *hostName, char *port)
 			 
 			  printf("server_recv_fileSize  filePointer %lld\n",  filePointer);
 
-			 if(!readBytes)
+			  if(!readBytes)
 				break;
-										// send num bytes already received
+										// send num bytes already received (filePointer)
 			  if(send(workSock, (char*)&filePointer, sizeof(long long), 0) < 0)										
 			  {
 			    perror("func send");
 			    return -1;
 			  } 
-			   printf("server_send %d\n",  readBytes);
+			   printf("server_send num bytes: %d, mess: %lld\n",  readBytes, filePointer);
 			}			
 			else
 			{
-			if((readBytes = recv((listenSock), (char*)&buf, BUFFER_SIZE, 0)) < 0)
-										// receive data from client
-			{
-				perror("func recv");
-				return -1;
-			}
-			printf("server_readbytes %d\n", readBytes);
-			if(!readBytes)
-				break;
-			filePointer = ftell(file);	
+			  if((readBytes = recv(workSock, (char*)&buf, BUFFER_SIZE, 0)) < 0)
+										  // receive data from client
+			  {
+				  perror("func recv");
+				  return -1;
+			  }
+			  printf("server_readbytes %d\n", readBytes);
+			  //if(!readBytes)
+			  //	  break;
+			  filePointer = ftell(file);	
 			  printf("server_fwrite filePointer %lld\n",  filePointer);
 			  fwrite((char*)buf, readBytes, 1, file);
 			  filePointer = ftell(file);
@@ -216,20 +217,14 @@ int startClient(char *hostName, char *port, char *filePath)
 	ind+=2;
     	hostAddr.sin_family = AF_INET;
     	hostAddr.sin_port = htons(atoi(port));					// convert host byte order -> network byte order		
-	if(!htons(inet_aton(hostName, hostAddr.sin_addr.s_addr)))		// new func convert IPv4 char* -> IPv4 bin (+ host byte order -> network byte order too) 
-	{
-		perror("func inet_aton");
-        	return -1;		
-	}
-    					
+	hostAddr.sin_addr.s_addr = inet_addr(hostName);				// old func convert IPv4 char* -> IPv4 bin (+ host byte order -> network byte order too) 
 	puts(filePath);
 	filePath[strlen(filePath)] = '\0';
 	file = fopen(filePath, "r");						// open file for read
 	fseek(file, 0L, SEEK_END);						
 	fileSize = ftell(file);							// get file size
 	fseek(file, 0L, SEEK_SET);						
-	fd_set temp;
-		
+	fd_set temp;		
 	struct timeval time_out; time_out.tv_sec = 0; time_out.tv_usec = 0;
 	while(1)								// if any key is pressed -> exit from while loop
     	{		
@@ -238,14 +233,13 @@ int startClient(char *hostName, char *port, char *filePath)
 			perror("func socket");
 			return -1;
 		}    
-		//hostAddr.sin_addr.s_addr = inet_addr(hostName);			// old func convert IPv4 char* -> IPv4 bin (+ host byte order -> network byte order too) 
+		//hostAddr.sin_addr.s_addr = inet_addr(hostName);		// old func convert IPv4 char* -> IPv4 bin (+ host byte order -> network byte order too) 
 		setsockopt(listenSock, SOCK_STREAM, 
 							SO_REUSEADDR, &so_reuseaddr, sizeof so_reuseaddr);
-											// reuse ADDR when socket in TIME_WAIT condition
+										// reuse ADDR when socket in TIME_WAIT condition
 		
 		
-		FD_ZERO (&temp);
-		FD_SET (listenSock,&temp);				  
+						  
 		puts("client_connect");
 		if(connect(listenSock, (struct sockaddr*) &hostAddr, sizeof(hostAddr)) < 0)
 		{
@@ -258,26 +252,31 @@ int startClient(char *hostName, char *port, char *filePath)
 		{
 		  perror("func send filePath");
 		  return -1;
-		}   								// send fileSize
+		}   								
 				
-		
+		FD_ZERO (&temp);
+		FD_SET (listenSock,&temp);
 		printf("client_fseekFilePointer_CUR %lld\n", filePointer);
 		fseek(file, filePointer, SEEK_CUR);				
 		
-		if(send(listenSock, (char*)&fileSize, sizeof(long long), 0) < 0)
+		printf("client_send_fileSize %lld\n", fileSize);
+		if(send(listenSock, (char*)&fileSize, sizeof(long long), 0) < 0)// send fileSize
 		{
 		  perror("func send fileSize");
 		  return -1;
-		} 								// get filePointer from client
+		} 								
 		
 		puts("client_recv_filePointer");
+										// get filePointer from client
 		if(recv(listenSock,(char*)&filePointer, sizeof(long long), 0) < 0)
 		{									      			  
 		  perror("func recv filePointer");
 		  return -1;
 		}		
-		printf("client_send_fileSize %lld\n", fileSize);
-		while((readBytes = fread(&buf, sizeof(char), BUFFER_SIZE, file)) > 0 && (fileSize-filePointer))
+		
+		printf("client_recvFilePointer_fromServer %lld\n", filePointer);
+		
+		while((readBytes = fread(&buf, sizeof(char), BUFFER_SIZE, file)) > 0)
 		{
 		  filePointer = ftell(file);
 		  printf("client_send_fread %d\n", readBytes);
